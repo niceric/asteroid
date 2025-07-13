53df356befc23e5600069020f570c0eec43843d0
NicEric <niclasericsson89@hotmail.com>
2025-07-13 20:12:05 +0200

    added delta time

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(screen)
     running = True
+    Clock = pygame.time.Clock()
+    dt = 0
     while running: 
+        print(dt)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -16,6 +19,6 @@ def main():
         pygame.display.flip()
         
         pass
-
+        dt = Clock.tick(FPS) / 1000
 if __name__ == "__main__":
     main()
